4a770ffc3279e889c3e63f2d4b50ac35c2211b8d
Alberto López <engineer.lopezx2@gmail.com>
2020-06-18 20:14:08 -0500

    ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// const element = document.createElement('h1');
+// element.innerText = 'Hola, Platzi badges 2';
+// const container = document.getElementById('app');
+// container.appendChild(element);
 
-const container = document.getElementById("app");
-const element = <h1>Hello, Platzi Badges</h1>;
+import React from 'react'; //Sera el analogo a createElement
+import ReactDOM from 'react-dom'; // Sera el analogo a appendChild
 
-ReactDOM.render(element, container);
+const container = document.getElementById('app');
+const element = <h1>Hello, Platzi 2 update</h1>; // Esto es JSX, y para poder utilizarlo SIEMPREEEEE hay que importar 'react'
+
+ReactDOM.render(element, container); // Primer dato que queremos renderizar y el segundo es donde lo queremos renderizar
