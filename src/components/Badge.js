9c4ed8ff6e1104373da4b60c744e11a7050d9cfb
Alberto López <engineer.lopezx2@gmail.com>
2020-06-19 23:29:50 -0500

    Agregando estilos a nuestro primer Componente, importando Bootstrap

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,28 +1,33 @@
 import React from 'react';
 
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
 import fotoPerfil from '../images/perfil.jpeg';
+import logoTwitter from '../images/logoTwitter.svg';
 
 class Badge extends React.Component {
   // Define el resultado que vamos a ver en pantalla
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo Conferencia" />
         </div>
-        <div>
-          <img src={fotoPerfil} alt="Avatar" />
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src={fotoPerfil} alt="Avatar" />
           <h1>
             Alberto <br />
-            Lopez
+            López
           </h1>
         </div>
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@lopezx2</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Engineer</h3>
+          <h4>
+            <img src={logoTwitter} alt="Logo Twitter" />
+            @lopezx2
+          </h4>
         </div>
-        <div>#PlatziConf</div>
+        <div className="Badge__footer">#PlatziConf</div>
       </div>
     );
   }
