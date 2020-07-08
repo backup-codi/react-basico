be05238cdf5ea2957a3507b49120082b7db414fd
Alberto López <engineer.lopezx2@gmail.com>
2020-07-08 17:53:35 -0500

    UI Components y Container Components
    En la programación es bueno separar las tareas en diferentes funciones y en React sucede lo mismo. Cuando un componente hace demasiado, probablemente es mejor dividirlo en dos.
    
    Esta técnica de componentes presentacionales y componentes container es común, útil y hace parte de las buenas prácticas.

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,93 +1,59 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import './styles/BadgeDetails.css';
 import LogoConfetencia from '../images/platziconf-logo.svg';
-import PageLoading from '../components/PageLoading';
-import PageError from '../components/PageError';
-import api from '../api';
 import Badge from '../components/Badge';
 import { Link } from 'react-router-dom';
 
-class BadgeDetails extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    data: undefined,
-  };
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = async () => {
-    this.setState({ loading: true, error: null });
-
-    try {
-      const data = await api.badges.read(this.props.match.params.badgeId);
-      this.setState({ loading: false, data: data });
-    } catch (error) {
-      this.setState({ loading: false, error: error });
-    }
-  };
-
-  render() {
-    if (this.state.loading) {
-      return <PageLoading />;
-    }
-    if (this.state.error) {
-      return <PageError error={this.state.error} />;
-    }
-
-    const badge = this.state.data;
-
-    return (
-      <Fragment>
-        <div className="BadgeDetails__hero">
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <img src={LogoConfetencia} alt="Logo de conferencia" />
-              </div>
-              <div className="col-6 BadgeDetails__hero-attendant-name">
-                <h1>
-                  {badge.firstName} {badge.lastName}
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
+function BadgeDetails(props) {
+  const badge = props.badge;
+  return (
+    <React.Fragment>
+      <div className="BadgeDetails__hero">
         <div className="container">
           <div className="row">
-            <div className="col">
-              <Badge
-                firstName={badge.firstName}
-                lastName={badge.lastName}
-                email={badge.email}
-                jobTitle={badge.jobTitle}
-                twitter={badge.twitter}
-              />
+            <div className="col-6">
+              <img src={LogoConfetencia} alt="Logo de conferencia" />
             </div>
-            <div className="col actions">
-              <h2>Actions</h2>
+            <div className="col-6 BadgeDetails__hero-attendant-name">
+              <h1>
+                {badge.firstName} {badge.lastName}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Badge
+              firstName={badge.firstName}
+              lastName={badge.lastName}
+              email={badge.email}
+              jobTitle={badge.jobTitle}
+              twitter={badge.twitter}
+            />
+          </div>
+          <div className="col actions">
+            <h2>Actions</h2>
+            <div>
               <div>
-                <div>
-                  <Link
-                    className="btn btn-primary"
-                    to={`/badges/${badge.id}/edit`}
-                  >
-                    Edit
-                  </Link>
-                </div>
-                <div>
-                  <button className="btn btn-danger">Delete</button>
-                </div>
+                <Link
+                  className="btn btn-primary"
+                  to={`/badges/${badge.id}/edit`}
+                >
+                  Edit
+                </Link>
+              </div>
+              <div>
+                <button className="btn btn-danger">Delete</button>
               </div>
             </div>
           </div>
         </div>
-      </Fragment>
-    );
-  }
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default BadgeDetails;
